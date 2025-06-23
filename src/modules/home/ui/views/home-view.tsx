9b69309fa82964909017b4f82aa8723eb69e01c0
Priyanshu Saxena <priyanshusaxena694@gmail.com>
2025-06-23 13:34:22 +0530

    agents-setup

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const { data } = useQuery(trpc.hello.queryOptions({text:""}));
-
-  return <div className="flex flex-col p-4 gap-4"></div>;
+  return <div>Home View</div>;
 };
